631d444dac905f1a01d6c5ffde34b46aace80442
rmattson1008 <rachelmattson1008@gmail.com>
2022-08-25 10:35:39 -0400

    clean code

--- src/inside_outside.py
+++ src/inside_outside.py
@@ -28,7 +28,8 @@ class Chart():
         self.arr[i, j, rule_idx] += value
         return
 
-    def print(self, num_slices=1):
+    def print_max(self, num_slices=1):
+        """Not a great function, for debugging"""
         print("shape:", self.arr.shape)
         if num_slices <= 0:
             num_slices = self.v
@@ -52,23 +53,26 @@ class Chart():
 
 
 def inside(words, args, g) -> Chart:
-    # print("ENTERING INSIDE")
-    # print(type(args.unary_rules), type(args.binary_rules))
+    """
+    Calculate the inside weights (sometimes called alphas) for the inside-outside algorithm.
+    Traverse possible grammer trees from a bottom up approach.
+    Produce a chart to keep track of wieghts where the "top-most" weight is Z, the partition function.
 
-    chart = []
-    # words = sent.split(" ")
-    # print(words)
+    Parameters:
+        words: tokenized sentene to parse
+        args: args
+        g: dict containing current weights of rules
+
+    Returns:
+        Chart containing inside weights
+    """
+
     n = len(words)
     num_unary_rules_used = 0
     chart = Chart(args.nts, args.nts2idx, args.unary_rules, args.binary_rules, words)
 
-    # TODO- where is good?
-    # print("pretty" in words)
     unary = [x for x in args.unary_rules if x[1] in words]
 
-
-    
-    # v = max(len(unary), len(args.binary_rules))
     # traverse unary rules
     for k in range(n):
         # gather list of rules to examine
@@ -81,50 +85,51 @@ def inside(words, args, g) -> Chart:
             chart.set(k, k+1, rule[0], value=prob)
 
 
-    # examined = []
     for sub_string_length in range(2, n+1):
-        # print("substring length", sub_string_length)
-        # print("L", l)
         for start_idx in range(n-sub_string_length+1):
-            # start_idx +=1
-            # end_idx = start_idx + sub_string_length+1
             end_idx = start_idx + sub_string_length
-            # print("Chart span", start_idx, end_idx)
-            for mid_idx in range(start_idx + 1, end_idx):  # IDX???????
-                # print("subtrees:", start_idx, mid_idx, end_idx)
-                # examined.append((start_idx, end_idx))
+            for mid_idx in range(start_idx + 1, end_idx):  
                 for rule in args.binary_rules:
                     A, B, C = rule
+                    ## Debugging:
                     # if g[rule] > 1.0:
                         # print(g[rule])
                         # exit()
                     new_prob = g[rule] * chart.get(start_idx,
                                                    mid_idx, B) * chart.get(mid_idx, end_idx, C)
-                    # new_prob = chart.get(start_idx, mid_idx, B) * chart.get(mid_idx, end_idx, C)
                     chart.add(start_idx, end_idx, A, value=new_prob)
-                    # chart.set(start_idx, end_idx, A, value=new_prob)
 
-    # chart.print(num_slices=-1)
     return chart
 
 
 def outside(sent, args, g) -> "tuple[dict, float]":
-    # print("ENTERING OUTSIDE")
+    """Calculate the outside weights (sometimes called betas) for the inside-outside algorithm.
+    Traverse possible grammer trees from a bottom up approach.
+    Produce a chart to keep track of wieghts
+    Update expected counts for each rule using alphas and betas
+    
+    Parameters:
+        sent: The sentence to parse
+        args: args
+        g: dict of current rule weights 
+    
+    Returns:
+        counts: dict containing expected counts of each rule
+        Z: scalar value of partition function
+
+    """
     words = sent.split(" ")
-    # # words = sent
+    ## if you desire to truncate your sentences for the sake of time 
     # if len(words) > 5:
     #     words = words[:5]
     if len(words) <= 1:
         print("sent TOO SHORT:", sent, ":")
-    # print(words)
-        # return g
-    # words = sent
+
     n = len(words)
     v = len(args.nts)
 
     out_rules = {}
     for rule in args.binary_rules:
-        # print("Rule_name", rule[:-1])
         out_rules[rule] = 0.0
     for rule in args.unary_rules:
         out_rules[rule] = 0.0
@@ -132,11 +137,11 @@ def outside(sent, args, g) -> "tuple[dict, float]":
 
     inside_weights = inside(words, args, g)
 
-    Z = inside_weights.get(0, n, "S") # why would this ever be in the thousands. 
+    Z = inside_weights.get(0, n, "S") 
     try:
         assert Z > 0.0
     except AssertionError:
-        # I think if z is 0 I just take g instead of the calculated count
+        #  If z is 0 I just take g instead of the calculated count
         for rule in args.unary_rules:
             counts[rule] = g[rule]
             # counts[rule] = 0
@@ -144,25 +149,18 @@ def outside(sent, args, g) -> "tuple[dict, float]":
             counts[rule] = g[rule]
         return counts, Z
 
-    # return counts, 1
-
     out_weights = Chart(args.nts, args.nts2idx, args.unary_rules, args.binary_rules, words)
 
     out_weights.add(0, n, "S", value=1)
-    # examined = []
     # traverse binary rules
     for sub_string_length in reversed(range(2, n+1)):  # ???
         for start_idx in range(n-sub_string_length + 1):
-           # for start_idx in range(n-sub_string_length):
-            # start_idx +=1
             end_idx = start_idx + sub_string_length
-            # print("Chart span", start_idx, end_idx)
 
             for mid_idx in range(start_idx + 1, end_idx):  # IDX???????
-                # print("subtrees:", start_idx, mid_idx, end_idx)
-                # examined.append((start_idx, mid_idx, end_idx))
                 for rule in args.binary_rules:
                     A, B, C = rule
+                    ## Debugging:
                     # if g[rule] > 1.0:
                         # print(g[rule])
                         # exit()
@@ -174,7 +172,7 @@ def outside(sent, args, g) -> "tuple[dict, float]":
                         start_idx, end_idx, A) * g[rule] * inside_weights.get(start_idx, mid_idx, B)
                     out_weights.add(start_idx, mid_idx, B, probB)
                     out_weights.add(mid_idx, end_idx, C, probC)
-    #travers unary rules
+    #traverse unary rules
     for k in range(n-1):
         relevant_rules = [x for x in args.unary_rules if words[k] in x]
         # print("Chart span", k, k+1)
@@ -183,23 +181,12 @@ def outside(sent, args, g) -> "tuple[dict, float]":
             # print(rule)
             out_rules[rule] += out_weights.get(k, k+1, A)
 
-   
-            # counts[rule] = 0
-        # print("_____")
-        # print("NO TREE FOUND - Z:0")
-        # print("INSIDE")
-        # print(inside_weights.print())
-        # print("OUTSIDE")
-        # print(out_weights.print())
-
-        
-
+    # update counts
     for rule in args.unary_rules:
         counts[rule] = (1/Z) * out_rules[rule] * g[rule]
     for rule in args.binary_rules:
         counts[rule] = (1/Z) * out_rules[rule] * g[rule]
 
-    # out_weights.print()
     return counts, Z
 
 
